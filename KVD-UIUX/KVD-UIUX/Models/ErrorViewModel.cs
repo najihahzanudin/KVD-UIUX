e84005ee7460661df3d9d09dfd9c460cf31f5da0
NajihahZanudin <najihah.z@intensecomp.com>
2024-11-02 15:35:57 +0800

    initial commit

---
+++ KVD-UIUX/KVD-UIUX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KVD_UIUX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
